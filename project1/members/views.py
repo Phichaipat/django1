7662e7bea73a848b562c3b1831b1a51367a900f4
Phichaipat <6529011011@cdti.ac.th>
2024-09-16 16:06:57 +0700

    update

--- project1/members/views.py
+++ project1/members/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Member
 
 def members(request):
     template = loader.get_template('members.html')
@@ -9,3 +10,12 @@ def members(request):
 def profile(request):
     template = loader.get_template('profile.html')
     return HttpResponse(template.render())
+
+def members(request):
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
